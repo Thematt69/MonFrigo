d24479e4de1a8c618c9ff54304baab2ba14b41b4
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2024-01-13 23:46:53 +0100

    update flutter + show generic product

--- lib/pages/fridge/fridge_page.dart
+++ lib/pages/fridge/fridge_page.dart
@@ -1,53 +1,20 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import '../../blocs/bloc_provider.dart';
-import '../../blocs/firebase_storage_bloc.dart';
+import '../../blocs/firebase_store_bloc.dart';
 import '../../extension/build_context.dart';
-import '../../mock_values.dart';
 import '../../widgets/app_scaffold.dart';
+import '../../widgets/custom_builder.dart';
 
-class FridgePage extends StatefulWidget {
+class FridgePage extends StatelessWidget {
   const FridgePage({super.key});
-
-  @override
-  State<FridgePage> createState() => _FridgePageState();
-}
-
-class _FridgePageState extends State<FridgePage> {
-  static const double _bodyPaddingValue = 16;
-  static const double _spacingValue = 8;
-  static const double _borderValue = 8;
-
-  double _widthRelativeToScreen(BuildContext context) {
-    late int multiplicator;
-    if (context.mediaQuery.size.width <= 650) {
-      multiplicator = 2;
-    } else if (context.mediaQuery.size.width <= 950) {
-      multiplicator = 3;
-    } else if (context.mediaQuery.size.width <= 1250) {
-      multiplicator = 4;
-    } else if (context.mediaQuery.size.width <= 1550) {
-      multiplicator = 5;
-    } else if (context.mediaQuery.size.width <= 1850) {
-      multiplicator = 6;
-    } else {
-      multiplicator = 7;
-    }
-
-    return (context.mediaQuery.size.width -
-            (_bodyPaddingValue * multiplicator) -
-            (_borderValue * multiplicator) -
-            (_spacingValue * (multiplicator - 1))) /
-        multiplicator;
-  }
 
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
       index: 0,
       body: SingleChildScrollView(
-        padding: const EdgeInsets.all(_bodyPaddingValue),
+        padding: const EdgeInsets.all(16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -59,121 +26,30 @@ class _FridgePageState extends State<FridgePage> {
             const Text('Ajouter des produits'),
             const Text("Modifier la quantité d'un produit"),
             const Text('Supprimer des produits'),
-            Wrap(
-              spacing: _spacingValue,
-              runSpacing: _spacingValue,
-              children: fridgeProductsMock.map((product) {
-                return Card(
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    side: BorderSide(color: context.colorScheme.outline),
-                    borderRadius: const BorderRadius.all(Radius.circular(12)),
-                  ),
-                  child: SizedBox(
-                    width: _widthRelativeToScreen(context),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(12),
-                            topRight: Radius.circular(12),
-                          ),
-                          child: FutureBuilder<String?>(
-                            future:
-                                BlocProvider.of<FirebaseStorageBloc>(context)
-                                    .getDownloadUrl(product.uuid),
-                            builder: (context, snapshot) {
-                              if (snapshot.connectionState ==
-                                  ConnectionState.waiting) {
-                                return const Padding(
-                                  padding: EdgeInsets.all(16),
-                                  child: Center(
-                                    child: CircularProgressIndicator(),
-                                  ),
-                                );
-                              } else if (snapshot.hasData) {
-                                return CachedNetworkImage(
-                                  width: _widthRelativeToScreen(context),
-                                  height: _widthRelativeToScreen(context),
-                                  imageUrl: snapshot.data!,
-                                  progressIndicatorBuilder: (_, __, progress) =>
-                                      Padding(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Center(
-                                      child: CircularProgressIndicator(
-                                        value: progress.progress,
-                                      ),
-                                    ),
-                                  ),
-                                  errorWidget: (_, __, ___) => const Padding(
-                                    padding: EdgeInsets.all(16),
-                                    child: Center(
-                                      child: Icon(
-                                        Icons.image_not_supported_outlined,
-                                      ),
-                                    ),
-                                  ),
-                                );
-                              }
-                              return const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Center(
-                                  child: Icon(
-                                    Icons.image_not_supported_outlined,
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(product.name),
-                              const SizedBox(height: 8),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                mainAxisSize: MainAxisSize.max,
-                                children: [
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.remove_circle_outline_outlined,
-                                    ),
-                                  ),
-                                  Column(
-                                    children: [
-                                      Text(
-                                        '${product.number}',
-                                      ),
-                                      Text(
-                                        '${product.number * product.quantity} ${product.unit.label}',
-                                      ),
-                                    ],
-                                  ),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.add_circle_outline_outlined,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                        // TODO: add expiration date chip
-                      ],
-                    ),
-                  ),
+            const SizedBox(height: 16),
+            Text(
+              'Liste des produits :',
+              style: context.textTheme.titleMedium,
+            ),
+            CustomStreamBuilder(
+              stream: BlocProvider.of<FirebaseStoreBloc>(context)
+                  .onGenericProductsChange,
+              dataBuilder: (context, data) {
+                return ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    final product = data[index];
+                    return ListTile(
+                      leading: Icon(product.icon),
+                      title: Text(product.name),
+                      subtitle: product.description == null
+                          ? null
+                          : Text(product.description!),
+                    );
+                  },
                 );
-              }).toList(),
+              },
             ),
           ],
         ),
